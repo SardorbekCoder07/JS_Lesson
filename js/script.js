3706a7b2ea471b880b44fa7fb0a3f5694e14df2c
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-05 15:14:30 +0500

    add

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// const shop = {
+//     products: [
+//         { id: 1, productName: "Iphone 13", price: 4000 },
+//         { id: 2, productName: "Iphone 14", price: 5000 },
+//         { id: 3, productName: "Iphone 15", price: 6000 },
+//         { id: 4, productName: "Iphone 16", price: 7000 }
+//     ],
+//     orders: [],
+//     total: 0,
+//     buy(productId){
+//         const product=this.products.find(p=>p.id===productId);
+//         this.orders.push(product);
+//         this.total+=product.price;
+//         alert(`Sotib olindi: ${product.productName}, Narxi: ${product.price}$`);
+//         alert(`Umumiy narx: ${this.total}$`);
+//     },
+// }
+
+
+// const students=[
+//     {
+//         student:'ali',
+//         grades:[40,35,25,10]
+//     },
+//     {
+//         student:'guli',
+//         grades:[50,65,75,90]
+//     },
+//     {
+//         student:'Qudrat',
+//         grades:[15,10,25,10]
+//     },
+// ]
+// let arr=[];
+// for(let i=0;i<students.length;i++){
+//     let average=0;
+//     for(let j=0;j<students[i].grades.length;j++){
+//         average+=students[i].grades[j];
+//     }
+//     let costAverage=(average/4).toFixed().toString();
+//     arr.push(costAverage);
+   
+// }
+// console.log(arr.sort((a,b)=>b-a));
+
+// //average grades
+
