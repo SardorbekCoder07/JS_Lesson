14a83b0fdb3058623e621c61967d79cd69da875d
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-05 15:56:00 +0500

    add

--- js/script.js
+++ js/script.js
@@ -111,3 +111,14 @@
 // }
 // console.log(person.calcAge());
 // console.log(this);
+
+
+// const person = {
+//     name: "Sardor",
+//     age: 25,
+//     greet() {
+//       console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//     },
+//   };
+  
+//   person.greet(); 
