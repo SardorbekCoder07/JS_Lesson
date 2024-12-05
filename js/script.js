d3485dfbb71653c00f79457e9f7dad58d01afa02
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-05 15:44:06 +0500

    add

--- js/script.js
+++ js/script.js
@@ -39,9 +39,64 @@
 //     }
 //     let costAverage=(average/4).toFixed().toString();
 //     arr.push(costAverage);
-   
+
 // }
 // console.log(arr.sort((a,b)=>b-a));
 
 // //average grades
 
+// const laLiga=[
+//     {team:'Real Madrid',points:33},
+//     {team:'Barcelona',points:37},
+//     {team:'Atletico',points:32},
+//     {team:'Valencia',points:29},
+//     {team:'Sevilla',points:26},
+//     {team:'Celta',points:24},
+// ]
+// let highPointTema=laLiga.sort((a,b)=>b.points-a.points)
+
+
+// let a=highPointTema.map((i,index)=>{
+//     console.log(`${index+1}:${i.team} --- point:${i.points}`);
+// })
+
+// console.table(highPointTema);
+
+
+// const student = {
+//     name: "Ali",
+//     age: 20,
+// };
+
+// for (const key in student) {
+//     console.log(`${key}: ${student[key]}`);
+// }
+
+// const person=['sardorbek',1,2,3]
+// for(const i in person){
+//     console.log(`${i}`);
+// }
+
+// const numbers = [10, 20, 30];
+
+// for (const index in numbers) {
+//   console.log(`Index: ${index}, Value: ${numbers[index]}`);
+// }
+
+// const numbers = [10, 20, 30];
+
+// for (const jasmina in numbers) {
+//   console.log(jasmina);
+// }
+
+// const word = "JavaScript";
+
+// for (const char in word) {
+//   console.log(char);
+// }
+
+
+// let arr=[1,2,3,4,5,6,658,7,4,5,8,7]
+// for(let i of arr){
+//     console.log(i);
+// }
