632ad4647db5b286e2512d57f556f0bebb59b813
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-13 17:06:57 +0500

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-let num = 123;//number
-let bool = true;//boolean
-let str = String(num); //string
-console.log(str, typeof str);
-console.log(typeof String(Boolean));
+// let num = 123;//number
+// let bool = true;//boolean
+// let str = String(num); //string
+// console.log(str, typeof str);
+// console.log(typeof String(Boolean));
+
+// let num = 456;
+// let bool = false;
+// console.log(typeof false.toString());
+// // console.log(num.toString()); 
+// // console.log(bool.toString()); 
