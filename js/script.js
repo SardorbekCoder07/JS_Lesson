8697152ddad89630837f3d3a1c7a73a1a8cc7eb6
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-17 14:49:20 +0500

    add

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@
 // const original = { name: "Sardor", age: 25 };
 // const clone = Object.assign({city:"Tashkent"},original);
 // console.log(clone);
+
+
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { b: 3, c: 4 };
+// const merged = Object.assign({}, obj1, obj2);
+// console.log(merged);
