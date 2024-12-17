d6761d141d38990f3359f751ae3201cd81fce837
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-17 14:49:54 +0500

    add

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 // console.log(clone);
 
 
-// const obj1 = { a: 1, b: 2 };
-// const obj2 = { b: 3, c: 4 };
+// const obj1 = { a: 1, b: 5};
+// const obj2 = { b:2, c: 4 };
 // const merged = Object.assign({}, obj1, obj2);
 // console.log(merged);
