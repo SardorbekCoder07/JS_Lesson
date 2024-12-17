1ff777c62c1d96bb8b4b13bbc4ae2362bee35fbf
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-17 14:48:45 +0500

    add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 // const person = { name: "Sardor", age: 25, city: "Tashkent" }
 // console.log(Object.entries(person));
+
+// const original = { name: "Sardor", age: 25 };
+// const clone = Object.assign({city:"Tashkent"},original);
+// console.log(clone);
