4ace16a969c9cd228cdae4b7a0259cac3ee0964f
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-03 17:05:52 +0500

    add

---
+++ js/function_calling.js
@@ -0,0 +1,56 @@
+// function sayHello(name) {
+// 	return "Salom, " + name + "!"
+// }
+
+
+// function greetUser() {
+// 	const message = sayHello("Sardorbek")
+// 	console.log(message)
+// }
+
+// greetUser()
+
+// function multiply(a, b) {
+// 	return a * b
+// }
+
+// function add(a, b) {
+// 	return a + b
+// }
+
+// function calculate(a, b) {
+// 	const product = multiply(a, b)
+// 	const sum = add(a, b)
+// 	return `Yig'indisi: ${sum}, Ko'paytmasi: ${product}`
+// }
+
+// console.log(calculate(5, 3));
+// const qushish = (a, b) => {
+// 	return a + b
+// }
+// const kopaytirish = (a, b) => {
+// 	return a * b
+// }
+// const ayrish = (a, b) => {
+// 	return a - b
+// }
+// const bulish = (a, b) => {
+// 	return a / b
+// }
+
+// const calculate = (a, b, operation) => {
+// 	if (operation === "*") {
+// 		const kup = kopaytirish(a, b)
+// 		return kup
+// 	} else if (operation === "/") {
+// 		const bul = bulish(a, b)
+// 		return bul
+// 	} else if (operation === "+") {
+// 		const qush = qushish(a, b)
+// 		return qush
+// 	}else if (operation === "-") {
+// 		const ayir = ayrish(a, b)
+// 		return ayir
+// 	}
+// }
+// console.log(calculate(20,15,"+"))
