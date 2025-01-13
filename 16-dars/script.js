bef4dc36ed40e571b4918990f8bb6f0d385c2f6a
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-13 17:20:37 +0500

    add

--- 16-dars/script.js
+++ 16-dars/script.js
@@ -3,3 +3,19 @@
 
 // let arr=[1,2,3,4,5]
 // console.log(arr[4]);
+
+// let arr=[1,true,"string",null,undefined]
+// console.log(typeof arr);
+
+// arraylarni yaratishni ikki xil usuli mavjud 1 konstruktor yoki funksiya ikkinchisi default array yaratish
+
+// let arr=new Array([1,true,"string",null,undefined])
+// console.log(arr[4]);
+
+
+/* arrayga indeks qushish hamda mavjuda indeksni yangi qiymatga alamashtirish */
+
+// let fruits = ["Apple", "Orange", "Plum"]// asl massiy
+// fruits[3] = "Kiwi"//yangilangan massiv 
+// console.log(fruits);
+
