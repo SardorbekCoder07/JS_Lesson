48261256f9a121bf5a6f064fd67de6f9e8ac88e4
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-13 18:01:51 +0500

    add

--- 16-dars/script.js
+++ 16-dars/script.js
@@ -40,3 +40,42 @@
 // 	console.log(`${i+1}-element: ${names[i]}`);
 // }
 
+// let newArr=['ara']
+// let arr=['ara']
+// console.log(arr===newArr);
+
+/* problem soolving */
+/* 1-problem arr clone */
+
+// const array_Clone = (arr) => {
+// 	return arr.slice(0)
+// }
+// console.log(array_Clone([1, 2, 4, 0]))
+
+/* nested array */
+// let arr=[1,2,3,[4,5,6]]
+// console.log(arr[3][2]);
+
+/* 2 check arr  */
+
+// const is_Arr = (arr) => {
+// 	return Array.isArray(arr)
+// }
+// console.log(is_Arr('w3resource'))
+// console.log(is_Arr([1, 2, 4, 0]));
+
+
+/* 3 get first element the array */
+// const first = (arr, n = 1) => {
+// 	if (n < 0) {
+// 		return []
+// 	} else {
+// 		return arr.slice(0, n)
+// 	}
+// }
+
+// console.log(first([7, 9, 0, -2]))
+// console.log(first([], 3))
+// console.log(first([7, 9, 0, -2], 3))
+// console.log(first([7, 9, 0, -2], 6))
+// console.log(first([7, 9, 0, -2], -3)) 
