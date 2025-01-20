3d2075cddf0f306d2de3f6697630534a9a11f399
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-20 16:45:04 +0500

    ADD

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -1,7 +1,18 @@
-/* Object created */
-const person={
-	fisrtName:"Sardorbek",
-	secondName:"Sayfullayev",
-	age:21,
-	country:`Uzbekistan`
+/* const person=["Sardorbek","Sayfullayev",21,"Erkak","Alibek","Raxmanov",13,"Erkak"] */
+
+
+/* Object bu qiymat (data) ni key  va value sifatida saqlash uchun ishlatilinadi  */
+/* const obj_key = "Name"
+const person = {
+	firstName: "Sardorbek",
+	secondName: "Sayfullayev",
+	age: 21,
+	gender: "Erkak",
 }
+ */
+
+// const person=new Object();
+/* person.firstName="Sardorbek"
+person.secondName="Sayfullayev"
+person.age=21
+person.gender="Erkak" */
