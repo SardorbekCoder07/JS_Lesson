459de4b1d758f255be936766c99406daa2d0c7e7
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-20 17:07:51 +0500

    ADD

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -16,3 +16,37 @@ const person = {
 person.secondName="Sayfullayev"
 person.age=21
 person.gender="Erkak" */
+
+/* const person = {
+	firstName: "Sardorbek",
+	secondName: "Sayfullayev",
+	age: 21,
+	gender: "Erkak",
+}
+person.firstName = "Alibek"
+console.log(person)
+delete person.age
+delete person.secondName
+delete person.firstName
+delete person.gender
+ */
+/* let firstName = "Sardorbek"
+let secondName = "Sayfullayev"
+let age = 21
+let gender = "Erkak"
+
+const universal_obj = {
+	firstName: firstName,
+	secondName: secondName,
+	age: age,
+	gender: gender,
+	calculateAge:(year)=>{
+		return 2025-year
+	},
+	phoneNumbers:["+998942939449","+998942939448","+998942939549",["+SAsdA","+998942939448","+998942939549"]],
+	nested_obj:{
+		skils:["html","css","js"],
+	}
+}
+console.log(universal_obj.nested_obj.skils[0]); */
+
