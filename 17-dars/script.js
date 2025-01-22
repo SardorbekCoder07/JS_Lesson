836eed99fe97ad3e591fa88ecebaf19aa4f2c3ec
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-22 17:31:35 +0500

    add

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -6,3 +6,12 @@
 for(const i in person){
 	console.log(`${i}:${person[i]}`)
 } */
+
+const numbers = [10, 20, 30]
+
+for (const i in numbers) {
+	console.log(`Index: ${i}, Value: ${numbers[i]}`)
+}
+// for(let i=0;i<numbers.length;i++){
+// 	console.log(numbers[i]);
+// }
