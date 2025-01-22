3f65d4d7cdf6d48e5827614f3ecf1d8e4199db6e
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-22 17:52:32 +0500

    ADD

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -20,3 +20,49 @@ for (const i of numbers) {
 for (const char in word) {
 	console.log([char])
 } */
+
+
+/* const person = {
+	name1: "Sardor",
+	age: 25,
+	greet() {
+		console.log(`Hello, my name is ${this.age} and I am ${this.age} years old.`)
+	},
+}
+
+person.greet()  */
+
+/* const person = {
+	name: "Sardor",
+	greet() {
+		console.log(`Outer function: ${this.name}`)
+		function innerFunction() {
+			console.log(`Inner function: ${this.confirm('aminmisan')}`)
+		}
+
+		innerFunction()
+	},
+}
+
+person.greet() */
+/* 
+const person = {
+	name: "Sardor",
+	innerFunction () {
+		console.log(`Inner function: ${this.name}`)
+	}
+}
+
+person.innerFunction() 
+ */
+/* 
+const person = {
+	name: "Sardor",
+	greet() {
+		console.log(`Hello, ${this.name}`)
+	},
+}
+
+const fn = person.greet
+fn()
+ */
