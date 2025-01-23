ea1cab571b66705c8b3248827abdbdcd89a9e82a
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-23 15:35:04 +0500

    add

---
+++ katta_guruh/script.js
@@ -0,0 +1,38 @@
+// const anotherFn = function (passengerNum=1, costBilet=0) {
+
+// 	return console.log(passengerNum, costBilet);
+
+// }
+// anotherFn(undefined,undefined)
+/* const anotherFunc = () => {
+	const console = {
+		name: "John",
+		age: 23,
+		isMarried: false,
+		phone: "123456789",
+		place: "USA",
+		log() {
+			return console.log(...arguments)
+		}
+	}
+	const { name, age } = obj
+	console.log(name)
+
+	return console.log(age)
+}
+anotherFunc() */
+/* function operator(son1, son2, son3) {
+	return add(son1, son2) * minus(son3, son2)
+}
+const add = (son1, son2) => {
+	return son1 + son2
+}
+const minus = (son3, son2) => {
+	return son3 - son2
+}
+
+function gigantFN(a) {
+	return a**a
+}
+const parametr=operator(2, 3, 4);
+console.log(gigantFN(parametr)) */
