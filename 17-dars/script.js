eddf751e993d21272ac095e84d3e97d6d4de50eb
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-01-27 17:18:26 +0500

    add

--- 17-dars/script.js
+++ 17-dars/script.js
@@ -7,3 +7,16 @@
 // person.age = 30
 // delete person.name
 // console.log(person)
+
+// const person = { name: "Sardor", age: 25 }
+// // Object.seal(person)
+// // person.age = 30
+// // person.city = "Tashkent"
+// // console.log(person) 
+
+// const entries = Object.entries(person)
+
+// console.log(Object.fromEntries(entries))
+
+
+// console.log(person.hasOwnProperty("age"));
