daff49b71e9e848a10b3a38d792f733b6b230399
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-07 17:07:41 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -106,3 +106,11 @@ console.log(name, restDetails);
 console.log(sum(1, 2, 3))
 
 console.log(sum(1, 2, 3, 4)) */
+
+
+function multiply(multiplier, ...theArgs) {
+	return theArgs.map((element) => multiplier * element)
+}
+
+const arr = multiply(2, 15, 25, 42)
+console.log(arr)
