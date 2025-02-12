0d55146b5d66f67dd833367aa2d02bc0ce241a14
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-12 18:08:23 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -1,3 +1,7 @@
-console.log(parseInt("123px12"))
-console.log(parseFloat("123.45px"))
-console.log(parseInt("abc123")) 
+console.log(isNaN("12abc"))
+console.log(isNaN(123))
+console.log(isNaN(NaN))
+
+console.log(Number.isNaN(NaN))
+console.log(Number.isNaN("abc"))
+console.log(Number.isNaN(123)) 
