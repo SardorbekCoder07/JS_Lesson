2250b094ef2366e693747ac863b5df8379b8693d
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-14 18:19:31 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -6,11 +6,3 @@
 // console.log(Number.isNaN("abc"))
 // console.log(Number.isNaN(123))
 
-
-
-console.log(Math.ceil(4.7))
-console.log(Math.ceil(-4.7))
-
-
-console.log(Math.round(5.5))
-console.log(Math.round(4.4)) 
