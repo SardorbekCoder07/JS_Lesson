2d38b6acf7e84ab21ec9f308a91572787edccfcb
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-15 01:26:57 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -6,34 +6,18 @@
 // console.log(Number.isNaN("abc"))
 // console.log(Number.isNaN(123))
 
-class Person {
-	constructor(name, age) {
-		this.name = name
-		this._age = age
-	}
 
-	// Getter
 
-	get getAge() {
-		return `${this.name}ning yoshi :${this._age}`
-	}
 
-	//setter yoshni o'zgartiradi
-
-	set getAge(newAge) {
-		if(newAge<0){
-			console.log("Yosh manfiy bo'lishi mumkin emas")
-		}else{
-			this._age=newAge
-		}
-	}
+const carPrototype = {
+	start() {
+		console.log(`${this.brand} yurishni boshladi!`)
+	},
 }
 
-const person=new Person("Sardorbek",22)
-console.log(person.getAge);
-person.getAge=30
-console.log(person.getAge);
-person.getAge=-5
-console.log(person.getAge);
+const myCar = Object.create(carPrototype)
+console.log(myCar);
 
+myCar.brand = "Tesla" 
 
+myCar.start() 
