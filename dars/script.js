db3b1e16c59c498a007be346649b05a567f8a537
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-17 17:09:48 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -1,24 +1,7 @@
-let date = new Date()
+console.log("Boshlanish...")
 
-const formatDate = (localDate, current) => {
-	return new Intl.DateTimeFormat(current, {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
-		hour24: true
-	}).format(localDate)
-}
+setTimeout(() => {
+	console.log("3 sekund o'tdi!")
+}, 3000)
 
-let en=formatDate(date,'en-US')
-let ru=formatDate(date,'ru-RU')
-let uz=formatDate(date,'uz-UZ')
-let fr=formatDate(date,'fr-FR')
-let de=formatDate(date,'de-DE')
-console.log(en);
-console.log(ru);
-console.log(de);
-console.log(fr);
-console.log(uz);
+console.log("Kutilmoqda...")
