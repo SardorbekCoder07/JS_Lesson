17354a723d4d60f0d2b1c33d1ba86993a06cd0bf
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-18 19:10:08 +0500

    add the lesson

--- dars/script.js
+++ dars/script.js
@@ -1,15 +1,9 @@
-const hours = document.getElementById('hour')
-const minutes = document.getElementById('minute')
-const seconds = document.getElementById('second')
-
-const clock = () => {
-	const date = new Date()
-	const hour = date.getHours()
-	const minute = date.getMinutes()
-	const second = date.getSeconds()
-
-	hour<10?hours.textContent="0"+hour:hours.textContent=hour
-	minute<10?minutes.textContent = "0" + minute : minutes.textContent = minute
-	second < 10 ? seconds.textContent = "0" + second : seconds.textContent = second
+function myName(){
+	console.log(`My name:`,this)
 }
-setInterval(clock, 1000)
+
+const car={
+	name:'Cobalt',
+	year:2022,
+	sayName:myName
+}
