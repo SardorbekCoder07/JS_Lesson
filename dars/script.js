0ddd3e615d68bfc5fe82e136ae5acc881c587a54
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-18 19:33:48 +0500

    ADDD

--- dars/script.js
+++ dars/script.js
@@ -1,9 +1,23 @@
-function myName(){
+/* function myName(){
 	console.log(`My name:`,this)
 }
 
 const car={
 	name:'Cobalt',
 	year:2022,
-	sayName:myName
+	sayName:myName,
+	sayNameWindow:myName.bind(window),
+	info:function(model){
+		console.log(`Car name is ${this.name} and year is ${this.year}`)
+		console.log(`Model:${model}`)
+	}
 }
+
+const gentra={
+	name:"Gentra",
+	year:2020,
+}
+
+const carInfo=car.info.bind(gentra)
+carInfo('Gentra') */
+
