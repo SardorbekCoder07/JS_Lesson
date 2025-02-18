13aa40cb3f77581e9135594f67eb6ac753d31454
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-18 19:58:56 +0500

    add

--- dars/script.js
+++ dars/script.js
@@ -23,7 +23,7 @@ carInfo('Gentra') */
 
 //bind, call and aplly
 
-function myName() {
+/* function myName() {
 	console.log(`Car`, this)
 }
 //myName()
@@ -46,6 +46,18 @@ const onix = {
 	year: 2024
 }
 
-car.info.bind(onix)('Chevrolet','red')
+car.info.bind(onix)('Chevrolet','red') */
 //car.info.call(onix, 'Chevrolet',"red")
 //car.info.apply(onix,['Chevrolet',"Red"])
+
+
+
+let arr=[1,2,3,4,5]
+
+function plusTo(arr,n){
+	return arr.map(el=>{
+		return el+2
+	})
+}
+
+console.log(plusTo(arr,2))
