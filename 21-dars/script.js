a73cfbf3f9f399846fcb759b7594a84427ff6613
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-19 16:52:18 +0500

    add

--- 21-dars/script.js
+++
@@ -1,2 +0,0 @@
-console.log("Salom hammaga")
-console.log("Salom hammaga")
--- katta_guruh/script.js
+++
@@ -1,6 +0,0 @@
-const box=document.querySelector(".box")
-box.style.width="200px"
-box.style.height="200px"
-box.style.borderRadius="50%"
-box.style.backgroundColor="red"
-box.style.transform="rotate(45deg)"
