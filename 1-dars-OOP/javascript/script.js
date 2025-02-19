1c82cd71cf6f93d835b5f2f2aa6b9d06c138e250
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-19 17:23:40 +0500

    addd

--- 1-dars-OOP/javascript/script.js
+++ 1-dars-OOP/javascript/script.js
@@ -1,4 +1,4 @@
-const user1 = {
+/* const user1 = {
 	name: "Ali",
 	age: 25,
 	greet: function () {
@@ -6,7 +6,7 @@ const user1 = {
 	}
 }
 const user2 = {
-	name: "Bekzod",
+	name: "Bekzod"
 	age: 20,
 	greet: function () {
 		console.log(`Salom, mening ismim ${this.name}!`)
@@ -23,3 +23,22 @@ const user3 = {
 user1.greet()
 user2.greet()
 user3.greet()
+ */
+
+//constructor function 
+const User = function (name, age) {
+	this.name = name
+	this.age = age
+	this.toast = function () {
+		alert(`Hello, my name is ${this.name}, and I'm ${this.age} years old.`)
+	}
+	this.calcAge = function () {
+		console.log(`I was born in ${2025 - this.age} years`)
+	}
+}
+
+const sardor = new User("Sardorbek", 20)
+const bekzod = new User("Bekzod", 20)
+const ali = new User("Ali", 25)
+
+
