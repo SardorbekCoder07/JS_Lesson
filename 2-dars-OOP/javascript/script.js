542de2fa0fa1d7fdcbe031e4f6ff05529f9252b3
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-21 17:07:42 +0500

    add

--- 2-dars-OOP/javascript/script.js
+++ 2-dars-OOP/javascript/script.js
@@ -1,5 +1,5 @@
 'use strict'
-const Person = function (surname, name, age) {
+/* const Person = function (surname, name, age) {
 	this.name = name
 	this.surname = surname
 	this.age = age
@@ -22,5 +22,17 @@ const sardor = new Person('Sayfullayev', "Sardorbek", 20)
 const sardo1r = new Person('Sayfullayev', "Sardorbek", 20)
 const jasmina = new People('Jurayeva', "Jasmina", 19)
 
-console.log(jasmina.__proto__===Person)
-console.log(jasmina)
+ */
+
+
+function Car(make, speed) {
+	this.make = make
+	this.speed = speed
+}
+
+Car.prototype.accelerate = function () {
+	this.speed += 10
+	console.log(`${this.make} endi ${this.speed} km/h tezlikda harakatlanmoqda`)
+}
+const bmw=new Car("BMW",200)
+console.log(bmw)
