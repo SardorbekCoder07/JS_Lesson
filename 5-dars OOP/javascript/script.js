2b3cab402d64b2b66cdfc4b948a63ce0c50606a1
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-28 15:32:15 +0500

    add

---
+++ 5-dars OOP/javascript/script.js
@@ -0,0 +1,57 @@
+'use strict'
+/* const Product={
+	init(name,price){
+		this.name=name
+		this.price=price
+	},
+	getInfo(){
+		console.log(`${this.name} narxi ${this.price}$`)
+	}
+}
+
+const ElectronicProduct=Object.create(Product)
+ElectronicProduct.initElectronic=function(name,price,brand){
+	this.init(name,price)
+	this.brand=brand
+}
+ElectronicProduct.initElectronic('MacBook Pro',1500,'Apple')
+ElectronicProduct.getInfo()
+
+
+const phone=Object.create(Product)
+phone.init('Iphone 15',1000,'Apple')
+phone.getInfo() */
+/* another class */
+class Account {
+	constructor(owner, currency, pin) {
+		this.owner = owner
+		this.currency = currency
+		this.pin = pin
+		this.movements = []
+		this.locale = navigator.language
+		console.log(`Thanks for opening an acccount ${this.owner}`)
+	}
+
+	deposit(val){
+		this.movements.push(val)
+	}
+	withdraw(val){
+		this.movements.push(-val)
+	}
+	approveLoan(val){
+		return true
+	}
+	requestLoan(val){
+		if(this.approveLoan(val)){
+			this.deposit(val)
+			console.log(`Loan approved`)
+		}
+	}
+}
+const acc1 = new Account("Sardorbek", "USD", 1111)
+console.log(acc1);
+
+
+
+
+
