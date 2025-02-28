1c654d6c560307a9a1ea6af2c41240b58d1bb233
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2025-02-28 17:18:41 +0500

    addd

--- 5-dars OOP/javascript/script.js
+++ 5-dars OOP/javascript/script.js
@@ -22,7 +22,7 @@ const phone=Object.create(Product)
 phone.init('Iphone 15',1000,'Apple')
 phone.getInfo() */
 /* another class */
-class Account {
+/* class Account {
 	constructor(owner, currency, pin) {
 		this.owner = owner
 		this.currency = currency
@@ -49,7 +49,7 @@ class Account {
 	}
 }
 const acc1 = new Account("Sardorbek", "USD", 1111)
-console.log(acc1);
+console.log(acc1); */
 
 
 
